175e9f3476988164b325d9d8ffe6a04ae171393e
kuliyhog <ylgoh516@gmail.com>
2020-02-19 17:49:58 +0000

    initial commit

---
+++ Assets/Scripts/ReferencePointManager.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+
+[RequireComponent(typeof(ARRaycastManager))]
+[RequireComponent(typeof(ARReferencePointManager))]
+[RequireComponent(typeof(ARPlaneManager))]
+
+public class ReferencePointManager : MonoBehaviour
+{
+    [SerializeField]
+    private Text debugLog;
+
+    [SerializeField]
+    private Text referencePointCount;
+
+    [SerializeField]
+    private Button togglePlaneDetectionButton;
+
+    [SerializeField]
+    private Button clearReferencePointButton;
+
+    private ARRaycastManager arRaycastManager;
+    private ARReferencePointManager arReferencePointManager;
+    private ARPlaneManager arPlaneManager;
+    private List<ARReferencePoint> referencePoints = new List<ARReferencePoint>();
+    private static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+    
+    void Awake(){
+        arRaycastManager = GetComponent<ARRaycastManager>();
+        arReferencePointManager = GetComponent<ARReferencePointManager>();
+        arPlaneManager = GetComponent<ARPlaneManager>();
+
+        togglePlaneDetectionButton.onClick.AddListener(TogglePlaneDetection);
+        clearReferencePointButton.onClick.AddListener(ClearReferencePoints);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount == 0){
+            return;
+        }
+
+        Touch touch = Input.GetTouch(0);
+
+        if (touch.phase != TouchPhase.Began){
+            return;
+        }
+
+        if (arRaycastManager.Raycast(touch.position, hits, TrackableType.PlaneWithinPolygon)){
+            Pose hitPose = hits[0].pose;
+            ARReferencePoint referencePoint = arReferencePointManager.AddReferencePoint(hitPose);
+
+            if (referencePoint == null){
+                string errorEntry = "There was an error creating a reference point";
+                Debug.Log(errorEntry);
+                debugLog.text += errorEntry;
+            }
+            else {
+                referencePoints.Add(referencePoint);
+                referencePointCount.text = $"Reference Point Count: {referencePoints.Count}";
+            }
+        }
+    }
+
+    private void TogglePlaneDetection() {
+        arPlaneManager.enabled = !arPlaneManager.enabled;
+
+        foreach (ARPlane plane in arPlaneManager.trackables)
+        {
+            plane.gameObject.SetActive(arPlaneManager.enabled);
+        }
+
+        togglePlaneDetectionButton.GetComponentInChildren<Text>().text = arPlaneManager.enabled ?
+            "Disable Plane Detection" : "Enable Plane Detection";
+
+    }
+
+    private void ClearReferencePoints(){
+        foreach(ARReferencePoint referencePoint in referencePoints)
+        {
+            arReferencePointManager.RemoveReferencePoint(referencePoint);
+        }
+        referencePoints.Clear();
+        referencePointCount.text = $"Reference Point Count: {referencePoints.Count}";
+    }
+    
+}
---
+++ Assets/Scripts/ToggleVisibility.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleVisibility : MonoBehaviour
+{
+
+    [SerializeField]
+    private Button toggleVisibilityButton;
+
+    [SerializeField]
+    private GameObject prefabHouse;
+
+    void Awake()
+    {
+        // add try catch phrase for null pointer exception -- not urgent
+        toggleVisibilityButton.onClick.AddListener(TogglePrefab);
+    }
+
+    private void TogglePrefab()
+    {
+        bool isActive = prefabHouse.activeSelf;
+        prefabHouse.SetActive(!isActive);
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
